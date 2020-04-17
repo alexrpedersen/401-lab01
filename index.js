4570807ee3005e7a25b3166ea809ffe6f13fd1e3
alexrpedersen <alex.r.pedersen@outlook.com>
2020-04-16 17:41:21 -0700

    adds models

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+  
 'use strict';
 
-const mongoose = require('mongoose');
-
+const Note = require('./lib/note.js');
 const Input = require('./lib/input.js');
-const Notes = require('./lib/notes');
+const mongoose = require('mongoose');
+const noteItem = require('./models/note-schema');
+const MONGODB_URI = "mongodb://test:test@127.0.0.1:27017/noterepo"
 
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+const input = new Input();
+const note = new Note(input);
+
+
+(input.valid())
+? note.execute().then(mongoose.disconnect).catch(e => console.log('***index***',e))
+: help();
+
+
+function help() {
+  console.log(`
+    USAGE: 
+    -a/--add - Add a note
+    -l/--list - List notes
+    -d/--delete - delete a note
+    `);
+  process.exit();
+}
